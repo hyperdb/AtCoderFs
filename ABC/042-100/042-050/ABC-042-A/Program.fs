7564d1e66752a3e27442d5c76f46e4a34989ceb8
hyperdb <hyperdb@gmail.com>
2024-06-07 13:24:44 +0900

    ✏️コメントを削除

--- ABC/042-100/042-050/ABC-042-A/Program.fs
+++ ABC/042-100/042-050/ABC-042-A/Program.fs
@@ -1,5 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-#nowarn "0025"
+﻿#nowarn "0025"
 let [|a;b;c|] = stdin.ReadLine().Split() |> Array.map int |> Array.sort
 
 if a + b + c = 17 && a = 5 && b = 5 then "YES" else "NO"
