1d2ee28a781f228f91dbc4e29ded532d26d7acaa
hyperdb <hyperdb@gmail.com>
2024-06-07 12:28:09 +0900

    ✨ABC-042-Aを追加

---
+++ ABC/042-100/042-050/ABC-042-A/Program.fs
@@ -0,0 +1,6 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+#nowarn "0025"
+let [|a;b;c|] = stdin.ReadLine().Split() |> Array.map int |> Array.sort
+
+if a + b + c = 17 && a = 5 && b = 5 then "YES" else "NO"
+|> stdout.WriteLine
