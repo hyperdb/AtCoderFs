1029ef1f1dd25a786a3f1b74b450bdbd6ed53c8b
hyperdb <hyperdb@gmail.com>
2024-06-07 13:24:58 +0900

    ✨ABC-042-Bを追加

---
+++ ABC/042-100/042-050/ABC-042-B/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+
+#nowarn "0025"
+let [|n;k|] = stdin.ReadLine().Split() |> Array.map int
+let e = [| for i=0 to n-1 do stdin.ReadLine() |> string |] |> Array.sort
+
+let result = e |> String.concat "" |> stdout.WriteLine
